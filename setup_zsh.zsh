1fd8b3975ec61cd8c849719fb4c5a818deb0e292
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2023-08-24 16:18:29 +0000

    Prevent superfluous command with cond

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,8 +4,27 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation Unnecessary; already done in the Brewfile
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo $(which zsh) | sudo tee -a /etc/shells >/dev/null
+if grep -Fxq "$(which zsh)" /etc/shells; then
+    echo "$(which zsh) is already in /etc/shells."
+else
+    echo "$(which zsh) is not in /etc/shells. Adding it now."
+    echo "Enter superuser (sudo) password to edit /etc/shells"
+    echo $(which zsh) | sudo tee -a /etc/shells >/dev/null
+fi
 
-echo "ENter user password to change login shell to zsh"
-chsh -s $(which zsh)
+if [ "$SHELL" = "$(which zsh)" ]; then
+    echo "ZSH is already the default shell."
+else
+    echo "ZSH is not the default shell. Changing it now."
+    echo "Enter user password to change login shell to zsh"
+    chsh -s $(which zsh)
+fi
+
+# Change default sh to zsh
+# if sh --version | grep -q 'zsh'; then
+#     echo "ZSH is already the default shell."
+# else
+#     echo "ZSH is not the default shell. Changing it now."
+#     echo "Enter user password to change login shell to zsh"
+#     chsh -s $(which zsh)
+# fi
