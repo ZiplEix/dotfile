4e42778c7c12fb9f6410a86760e859d4996ca860
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2023-08-24 16:07:28 +0000

    [ADD] redirecting stdout in /dev/null
    for /etc/shells modification on zsh setup

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,7 +5,7 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 # Installation Unnecessary; already done in the Brewfile
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo $(which zsh) | sudo tee -a /etc/shells
+echo $(which zsh) | sudo tee -a /etc/shells >/dev/null
 
 echo "ENter user password to change login shell to zsh"
 chsh -s $(which zsh)
